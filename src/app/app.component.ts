928b5f400688d0649868d60dcebe2e4d3fa6f104
Nandish <vknandish97@gmail.com>
2020-11-06 20:19:17 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse} from '@angular/common/http';
+import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Assignment-Frontend';
+  totalUsers: any;
+  chartData: number[]=[];
+  chartLabel: string[]=['Latitude >0','Latitude <0','Longitude >0','Longitude <0'];
+  apiData: any;
+  pieChartType:string = 'pie';
+  pieChartColors: Array < any > = [{
+    backgroundColor: ['#fc5858', '#19d863', '#fdf57d'],
+    borderColor: ['rgba(252, 235, 89, 0.2)', 'rgba(77, 152, 202, 0.2)', 'rgba(241, 107, 119, 0.2)']
+  }];
+  displayedColumns: string[] = ['Sno', 'name', 'username', 'city','pincode','company'];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void{
+
+    this.getFromApi();
+
+  }
+  async getFromApi(){
+    let lat_ge = 0;
+    let lat_le = 0;
+    let lon_ge = 0;
+    let lon_le = 0;
+    let count =0;
+     this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(async(response)=>{
+      console.log("Response",response)
+      this.apiData = response;
+      await this.apiData.forEach(element => {
+        count = count +1;
+        const lat = parseInt(element.address.geo.lat);
+        const lon = parseInt(element.address.geo.lng);
+        if(lat > 0){
+          lat_ge = lat_ge+1;
+        }else{
+          lat_le +=1;
+        }
+        if(lon > 0){
+          lon_ge +=1;
+        }else{
+          lon_le +=1;
+
+        }
+      });
+      console.log("gte",lat_ge,lon_ge)
+      console.log("lte",lat_le,lon_le)
+      this.chartData.push(lat_ge);
+      this.chartData.push(lat_le);
+      this.chartData.push(lon_ge);
+      this.chartData.push(lon_le)
+      this.totalUsers = count;
+      console.log("Chart data",this.chartData)
+    })
+
+  }
+
+  chartClicked(e:any):void {
+    console.log(e);
+  }
+  chartHovered(e:any):void {
+    console.log(e);
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from 'ng2-charts';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ChartsModule,
+    MatCardModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatTableModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
